38ebfbb422e010a5b19331f869c03b166bc2fa8d
Nick Kuzmik <nick@kuzmik.org>
2023-10-20 20:01:08 -0400

    adding some secrets for use in a coming script

--- bin/proxysql-admin.sh
+++ bin/proxysql-admin.sh
@@ -4,5 +4,7 @@ set -eou pipefail
 # Script to connect to the proxysql-core via mysql, without execing into the pod
 
 mysql_info=$(kubectl get service -n proxysql proxysql-core --output=json | jq -r '.spec.clusterIP, .spec.ports[0].port')
+mysql_host=$(echo "$mysql_info" | awk 'NR==1')
+mysql_port=$(echo "$mysql_info" | awk 'NR==2')
 
-mysql -h$(echo "$mysql_info" | awk 'NR==1') -P$(echo "$mysql_info" | awk 'NR==2') -uradmin -pradmin
+mysql -h"$mysql_host" -P"$mysql_port" -uradmin -pradmin
